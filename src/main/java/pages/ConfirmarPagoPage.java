cd6f3a6b6b88d0254367c2457fc6554168b6a8fb
dcastril <dcastril@bancolombia.com.co>
2018-06-16 22:13:47 -0500

    hacer adherencia de la confirmacion

--- src/main/java/pages/ConfirmarPagoPage.java
+++ src/main/java/pages/ConfirmarPagoPage.java
@@ -1,5 +1,40 @@
 package pages;
 
-public class ConfirmarPagoPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import util.MobilePageObject;
+
+public class ConfirmarPagoPage extends MobilePageObject {
+
+	@AndroidFindBy(id = "android:id/button1")
+	private WebElement yesButton;
+
+	@AndroidFindBy(id = "android:id/button2")
+	private WebElement noButton;
+
+
+	public ConfirmarPagoPage(WebDriver driver) {
+		super(driver);
+	}
+	
+	public boolean clickYes() {
+		try {
+			yesButton.click();
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+	
+	public boolean clickNo() {
+		try {
+			noButton.click();
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
 
 }
